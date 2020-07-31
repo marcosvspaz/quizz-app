0ed37b97e4cc4aa9af09956a908ef5c37c578e01
diegod3v <diegod3v@gmail.com>
2020-07-31 00:22:05 -0500

    add base dependencies, add linter

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        accent: 'var(--color-accent)',
+      },
+      height: {
+        '14': '3.6rem',
+        'screen-layout': 'calc(100vh - 3.6rem)'
+      },
+      spacing: {
+        '14': '3.6rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
